c0c207d1ecd38241734e7dfdeb0ae34451f31698
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-03-25 13:28:42 +0000

    examples: improve sole example

--- examples/js/main.js
+++ examples/js/main.js
@@ -1,23 +1,46 @@
-require( [ 'jquery' ], function ($) {
-  return MainModule(window, $);
+require( [ 'jquery', 'src/html_highlighter' ], function ($, hh) {
+  return MainModule(window, $, hh);
 } );
 
 
-MainModule = function (window, $, undefined) {
+var MainModule = function (window, $, hh, undefined) {
 
   var dataBaseUrl = 'data/',
-      dataSources = [ {
+      dataSources = [ /* { */
+/*         name: 'Two paragraphs', */
+/*         url: 'two-paras.json' */
+/*       }, */{
         name: 'Viber hacked',
-        url: 'viber-attacked-by-syrian-electronic-army.json?'
+        url: 'viber-attacked-by-syrian-electronic-army.json'
       } ];
 
   var elSelector = $('#filter-data'),
       elDocument = $('#document'),
-      elWidget = $('#widget');
+      elWidget = $('#widget'),
+      elSearch = $('#search'),
+      elAdd = $('#add');
+
+  var count = 0,
+      highlighter;
 
 
   function init()
   {
+    elSearch.keyup(function () {
+      elAdd.attr('disabled', elSearch.val().trim().length === 0);
+    } );
+    elSearch.val('').trigger('keyup').focus();
+
+    elAdd.click(function () {
+      var name = 'search#' + ++count;
+      console.log('Creating search query: name=%s | query=%s',
+                  name,
+                  elSearch.val());
+
+      highlighter.add(name, [ elSearch.val() ]);
+      elSearch.select().focus();
+    } );
+
     elSelector
       .children().remove()
       .change(function () {
@@ -28,16 +51,24 @@ MainModule = function (window, $, undefined) {
     var next = function (i) {
       if(i >= dataSources.length) {
         load(0);
+
+        highlighter = new hh.HtmlHighlighter( {
+          container: elDocument,
+          widget: elWidget
+        } );
         return;
       }
 
       var d = dataSources[i];
       elSelector.append($('<option/>').attr('value', i).html(d.name));
 
-      require( [ dataBaseUrl + d.url ], function (result) {
+      require( [ dataBaseUrl + d.url + '?' ], function (result) {
+        /* Note: due to obvious constraints imposed on loading of local system
+         * resources (via file://), data source files are required to export the
+         * global variable `g_descriptor´. */
         if(g_descriptor !== undefined) {
           d.content = g_descriptor;
-          g_descriptor = undefined;
+          delete g_descriptor;
           ++i;
         } else
           dataSources.splice(i, 1);
