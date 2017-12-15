78904e1cd856e00b6fe338dfa039371d0c4d07c3
Miguel Guedes <miguel.a.guedes@gmail.com>
2017-12-15 16:09:37 +0000

    examples: fixes & improvements
    * Prettier JS applied
    * Fixes incorrect module import

--- examples/monolith/main.js
+++ examples/monolith/main.js
@@ -1,129 +1,154 @@
-import $ from "jquery";
+import $ from 'jquery';
 import {
-  HtmlHighlighter, HtmlXpathFinder, HtmlRangeHighlighter
-} from "../../src/main.js";
-import "./theme.css";
+  HtmlHighlighter,
+  XPathFinder,
+  RangeHighlighter,
+} from '../../src/main.js';
+import './theme.css';
 
 /* eslint-disable global-require */
 const dataFiles = [
-  "viber_attacked_by_syrian_electronic_army",
-  "viber_attacked_by_syrian_electronic_army-cropped",
-  "ars_technica",
-  "simple",
-  "spaces",
-  "one_paragraph",
-  "one_paragraph-ampersand",
-  "one_paragraph-ampersand_escaped",
-  "one_paragraph-ampersand_nonexistent",
+  'viber_attacked_by_syrian_electronic_army',
+  'viber_attacked_by_syrian_electronic_army-cropped',
+  'ars_technica',
+  'simple',
+  'spaces',
+  'one_paragraph',
+  'one_paragraph-ampersand',
+  'one_paragraph-ampersand_escaped',
+  'one_paragraph-ampersand_nonexistent',
 ];
-const data = dataFiles.map((d) => require("../../etc/data/" + d).html);
+const data = dataFiles.map(d => require('../../etc/data/' + d).html);
 
 const MAX_HIGHLIGHT = 5;
 const dataSources = [
-  {name: "Viber hacked"},
-  {name: "Viber hacked -- cropped"},
-  {name: "Ars Technica"},
-  {name: "Simple"},
-  {name: "Spaces"},
-  {name: "One paragraph"},
-  {name: "One paragraph with ampersand"},
-  {name: "One paragraph with escaped ampersand"},
-  {name: "One paragraph with nonexistent ampersand"}
+  { name: 'Viber hacked' },
+  { name: 'Viber hacked -- cropped' },
+  { name: 'Ars Technica' },
+  { name: 'Simple' },
+  { name: 'Spaces' },
+  { name: 'One paragraph' },
+  { name: 'One paragraph with ampersand' },
+  { name: 'One paragraph with escaped ampersand' },
+  { name: 'One paragraph with nonexistent ampersand' },
 ];
 /* eslint-enable global-require */
 
-var $selector, $document, $widgetSelection, $widgetMain, $search, $add;
+let $selector, $document, $widgetSelection, $widgetMain, $search, $add;
 
-var count = 0,
-    mouseDown = 0,
-    highlighter;
+let count = 0;
+let mouseDown = 0;
+let highlighter;
 
-
-function init()
-{
-  $selector = $("#filter-data");
-  $document = $("#document");
-  $widgetSelection = $("#widget-selection");
-  $widgetMain = $("#widget-main");
-  $search = $("#search");
-  $add = $("#add");
+function init() {
+  $selector = $('#filter-data');
+  $document = $('#document');
+  $widgetSelection = $('#widget-selection');
+  $widgetMain = $('#widget-main');
+  $search = $('#search');
+  $add = $('#add');
 
   /* Set-up sequence
    * --
    * UI */
-  $search.keyup(function () {
-    $add.attr("disabled", $search.val().trim().length === 0);
+  $search.keyup(function() {
+    $add.attr('disabled', $search.val().trim().length === 0);
   });
-  $search.val("").trigger("keyup").focus();
+  $search
+    .val('')
+    .trigger('keyup')
+    .focus();
 
-  $add.click(function () {
-    var name = $search.val();
+  $add.click(function() {
+    const name = $search.val();
     highlighter.add(name, [name], true).apply();
     $search.select().focus();
   });
 
   $selector
-    .change(function () { load(parseInt($selector.val())); })
-    .children().remove();
+    .change(function() {
+      load(parseInt($selector.val(), 10));
+    })
+    .children()
+    .remove();
 
-  var timeout = null;
+  let timeout = null;
   $document.on({
-    dblclick: function () { mouseDown = 0; },
-    mouseup: function () {
+    dblclick: function() {
+      mouseDown = 0;
+    },
+    mouseup: function() {
       --mouseDown;
 
       /* Process text selection with a delay to ensure accurate results. */
-      if(timeout !== null) window.clearTimeout(timeout);
-      timeout = window.setTimeout(function () {
+      if (timeout !== null) window.clearTimeout(timeout);
+      timeout = window.setTimeout(function() {
         timeout = null;
-        if(mouseDown !== 0) return;
+        if (mouseDown !== 0) return;
 
-        var range = highlighter.getSelectedRange();
-        if(range === null) {
-          $widgetSelection.removeClass("enabled");
+        const range = highlighter.getSelectedRange();
+        if (range === null) {
+          $widgetSelection.removeClass('enabled');
           return;
         }
 
-        $widgetSelection.find(".offset").text(
-          range.start.marker.offset
-            + "(" + range.start.offset + ")"
-            + ":" + range.end.marker.offset
-            + "(" + range.end.offset + ")");
+        $widgetSelection
+          .find('.offset')
+          .text(
+            range.start.marker.offset +
+              '(' +
+              range.start.offset +
+              ')' +
+              ':' +
+              range.end.marker.offset +
+              '(' +
+              range.end.offset +
+              ')'
+          );
 
-        var xpath = range.computeXpath();
-        $widgetSelection.find(".xpath")
-          .text(xpath.start.xpath + ":" + xpath.start.offset
-                + " - " + xpath.end.xpath + ":" + xpath.end.offset);
+        const xpath = range.computeXpath();
+        $widgetSelection
+          .find('.xpath')
+          .text(
+            xpath.start.xpath +
+              ':' +
+              xpath.start.offset +
+              ' - ' +
+              xpath.end.xpath +
+              ':' +
+              xpath.end.offset
+          );
 
         highlight_(xpath.start, xpath.end);
         highlighter.clearSelectedRange();
-        $widgetSelection.addClass("enabled");
+        $widgetSelection.addClass('enabled');
       }, 150);
     },
-    mousedown: function () {
+    mousedown: function() {
       ++mouseDown;
-    }
+    },
   });
 
   dataSources.forEach((d, i) => {
-    let $opt = $("<option/>").attr("value", i).html(d.name),
-        c = data[i];
+    const $opt = $('<option/>')
+      .attr('value', i)
+      .html(d.name);
+    const c = data[i];
 
-    if(typeof c !== "string" || c.length <= 0) $opt.prop("disabled", true);
+    if (typeof c !== 'string' || c.length <= 0) $opt.prop('disabled', true);
     $selector.append($opt);
   });
 
   highlighter = new HtmlHighlighter({
     container: $document,
     widget: $widgetMain,
-    maxHighlight: MAX_HIGHLIGHT
+    maxHighlight: MAX_HIGHLIGHT,
   });
 
   load(0);
 }
 
-function load(index)
-{
+function load(index) {
   highlighter.clear().apply();
   $document.html(data[index]);
   highlighter.refresh();
@@ -132,18 +157,15 @@ function load(index)
 
 /* TODO: the following is a nasty hack which was quickly written as a proof
  * of concept and is thus NOT meant to be used in real applications. */
-function highlight_(start, end)
-{
-  var hit,
-      finder = new HtmlXpathFinder(
-        highlighter.content,
-        { start: start, end: end });
+function highlight_(start, end) {
+  const finder = new XPathFinder(highlighter.content, {
+    start: start,
+    end: end,
+  });
+  let hit;
 
-  if((hit = finder.next()) !== false)
-    new HtmlRangeHighlighter(count).do(hit);
-
-  if(++count >= MAX_HIGHLIGHT)
-    count = 0;
+  if ((hit = finder.next()) !== false) new RangeHighlighter(count).do(hit);
+  if (++count >= MAX_HIGHLIGHT) count = 0;
 }
 
 /* Run! */
