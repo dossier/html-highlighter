c00e2b7825c3eb4e69eba35b6b087ac1d4b9e7a2
Miguel Guedes <miguel.a.guedes@gmail.com>
2018-01-05 07:06:00 +0000

    Fix broken build of monolith example in server mode

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,35 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const env = process.env;  // eslint-disable-line no-process-env
 const isProduction = env.NODE_ENV === "production";
 
+function makeExampleConfig(name) {
+  return  {
+    entry: {
+      // We always assume that the example's entry filename is `main.js`
+      monolith: `./examples/${name}/main.js`,
+    },
+    output: {
+      path: path.join(__dirname, "dist/examples", name),
+      filename: "[name].js",
+    },
+    module: {
+      rules: [
+        jsLoader,
+        { test: /\.css$/, loader: "style-loader!css-loader" },
+        { test: /\.json$/, loader: "json-loader" },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        chunks: ["monolith"],
+        title: "HTML Highlighter -- main example",
+        template: "./examples/monolith/main.html",
+        filename: "index.html",
+      }),
+    ],
+    devtool: "#source-map",
+  };
+}
+
 const linters = [
   {
     enforce: "pre",
@@ -55,40 +84,6 @@ const lib = {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(true),
     new webpack.BannerPlugin(fs.readFileSync("./LICENSE", "utf8")),
-  ],
-  devtool: "#source-map",
-};
-
-const examples = {
-  entry: {
-    monolith: "./examples/monolith/main.js",
-  },
-  output: {
-    path: path.join(__dirname, "dist/examples"),
-    filename: "[name]/[name].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        query: {
-          presets: ["es2015"],
-          cacheDirectory: true,
-        },
-      },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.json$/, loader: "json-loader" },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      chunks: ["monolith"],
-      title: "HTML Highlighter -- main example",
-      template: "./examples/monolith/main.html",
-      filename: "monolith/index.html",
-    }),
   ],
   devtool: "#source-map",
 };
@@ -165,7 +160,10 @@ if(isProduction) {
   }));
 }
 
+const examples = [makeExampleConfig('monolith')];
 let build = [lib];
-if (!isProduction) build = build.concat([examples, assets, tests]);
+if (!isProduction) {
+  build = build.concat(examples, [assets, tests]);
+}
 
 module.exports = build;
