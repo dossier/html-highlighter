aafd4400825aa91fe1068929ad4a312c004ffa6f
Miguel Guedes <miguel.a.guedes@gmail.com>
2018-06-07 16:04:09 +0100

    hh: export client options typedef

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-// For whatever reason, it is not possible to use the handy `export * from "module"` syntax.
-import HtmlHighlighter from "./htmlhighlighter";
-import RangeHighlighter from "./rangehighlighter";
-import TextFinder from "./textfinder";
-import XPathFinder from "./xpathfinder";
+// @flow
 
-export {
-  HtmlHighlighter,
-  RangeHighlighter,
-  TextFinder,
-  XPathFinder,
-};
+// For whatever reason, it is not possible to use the handy `export * from "module"` syntax.
+import HtmlHighlighter from './htmlhighlighter';
+import RangeHighlighter from './rangehighlighter';
+import TextFinder from './textfinder';
+import XPathFinder from './xpathfinder';
+import type { ClientOptions } from './typedefs';
+
+export type { ClientOptions };
+export { HtmlHighlighter, RangeHighlighter, TextFinder, XPathFinder };
