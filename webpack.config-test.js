2129ba8f5ff1c5b0969f1d9266139a3f174f8b9d
Miguel Guedes <miguel.a.guedes@gmail.com>
2018-07-10 10:51:12 +0100

    Reformat double-quotes

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,29 +1,36 @@
-const webpack = require("webpack");
-const nodeExternals = require("webpack-node-externals");
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  target: "node",
+  target: 'node',
   externals: [nodeExternals()],
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
-      }, {
+        loader: 'eslint-loader',
+      },
+      {
         test: /.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
           cacheDirectory: true,
         },
-      }, {
-        test: /\.json$/, loader: "json-loader",
-      }, {
-        test: /\.css$/, loader: "css-loader",
-      }, {
-        test: /\.html$/, loader: "html-loader",
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
       },
     ],
   },
@@ -34,5 +41,5 @@ module.exports = {
       TEST: true,
     }),
   ],
-  devtool: "source-map",
+  devtool: 'source-map',
 };
