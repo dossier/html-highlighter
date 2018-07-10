ba338430cad3ea14524bdb1a8f3e61ba84d3427b
Miguel Guedes <miguel.a.guedes@gmail.com>
2018-07-10 09:19:13 +0100

    hh: ignore invisible highlights

--- src/highlightmarkers.js
+++ src/highlightmarkers.js
@@ -1,6 +1,7 @@
 // @flow
 
 import Range from './range';
+import * as dom from './dom';
 import type { QuerySet } from './typedefs';
 
 export type Marker = {|
@@ -75,15 +76,35 @@ class HighlightMarkers {
     }, 0);
   }
 
+  calculateTotalVisible(queryNames: Array<string> | null): number {
+    if (queryNames == null) {
+      return this.markers.reduce(
+        (acc, marker) => acc + Number(dom.isHighlightVisible(marker.id)),
+        0
+      );
+    }
+
+    return this.markers.reduce((acc, marker) => {
+      if ((queryNames: any).indexOf(marker.query.name) >= 0 && dom.isHighlightVisible(marker.id)) {
+        return acc + 1;
+      }
+
+      return acc;
+    }, 0);
+  }
+
   find(at: number, queryNames: Array<string> | null): Marker | null {
     let marker: Marker | null = null;
 
     this.markers.some(m => {
       const q = m.query;
 
+      // Queryset must be enabled and highlight visible
       if (!q.enabled) {
         return false;
       } else if (queryNames != null && queryNames.indexOf(q.name) < 0) {
+        return false;
+      } else if (!dom.isHighlightVisible(m.id)) {
         return false;
       } else if (at < 1) {
         marker = m;
