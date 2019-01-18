4f965c6a0e90cf90e953d63a7bf52da9f3695518
Miguel Guedes <miguel.a.guedes@gmail.com>
2019-01-18 11:17:14 +0000

    src: add promise utility function

--- src/util.js
+++ src/util.js
@@ -1,7 +1,24 @@
 // @flow
 
+export type PromiseCapabilities = {| instance: Promise<*>, resolve: any => any, fail: any => any |};
+
 function abstract() {
   throw new Error('Abstract method not implemented');
 }
 
-export { abstract };
+function createPromiseCapabilities(): PromiseCapabilities {
+  let resolve, fail;
+  const instance = new Promise((pr, pf) => {
+    resolve = pr;
+    fail = pf;
+  });
+
+  // $FlowFixMe: `resolve` and `fail` are defined at this point
+  return {
+    instance,
+    resolve,
+    fail,
+  };
+}
+
+export { abstract, createPromiseCapabilities };
